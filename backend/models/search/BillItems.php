97d2e3644af6064382ce4db8f1f812a6bb81d9ee
Chanpan Nuttaphon <chanpan.nuttaphon@gmail.com>
2019-09-06 10:46:10 +0700

    Update BillItems.php

--- backend/models/search/BillItems.php
+++ backend/models/search/BillItems.php
@@ -45,6 +45,9 @@ class BillItems extends BillItemsModel
 
         $dataProvider = new ActiveDataProvider([
             'query' => $query,
+            'pagination' => [
+                'pageSize' => 100,
+            ],
         ]);
 
         $this->load($params);
